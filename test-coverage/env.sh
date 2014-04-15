126669db7249af59fe2ceb9e71991490831cd267
Alex Leffelman <alex@remind101.com>
2014-04-14 22:20:52 -0700

    Removing env.sh from git tracking;

--- test-coverage/env.sh
+++
@@ -1,5 +0,0 @@
-export BUILT_PRODUCTS_DIR="/Users/alexleffelman/Library/Developer/Xcode/DerivedData/i2048-gljchvrffoiahddqoobfkbflrgsu/Build/Products/Debug-iphoneos"
-export CURRENT_ARCH="arm64"
-export OBJECT_FILE_DIR_normal="/Users/alexleffelman/Library/Developer/Xcode/DerivedData/i2048-gljchvrffoiahddqoobfkbflrgsu/Build/Intermediates/i2048.build/Debug-iphoneos/i2048.build/Objects-normal"
-export OBJROOT="/Users/alexleffelman/Library/Developer/Xcode/DerivedData/i2048-gljchvrffoiahddqoobfkbflrgsu/Build/Intermediates"
-export SRCROOT="/Users/alexleffelman/Dropbox/Code/i2048"
